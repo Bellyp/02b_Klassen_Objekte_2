c8ca342753934f416ccc9db1f18a76c1d7b97696
Petro <p.nikolaou@web.de>
2025-01-17 10:49:23 +0100

    1. / Felder / Wertzuweisung / toString()

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,19 @@ package com.cc.java;
 
 public class Cat {
 
+   String firstName; 
+   String furColor;
+   int age;
+
+
+   
+
+
+
+
+
+
+
 
     public Cat getInstanceVariable(){
        return this;
